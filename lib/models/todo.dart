13755faa2ac9b47ce29ab50c1c8c7dce3f4e001e
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2022-02-22 14:21:21 +0300

    todo model update

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,11 +1,18 @@
 class ToDo {
+  final int userId;
   final int id;
   final String title;
+  final bool completed;
 
-  ToDo(this.id, this.title);
+  ToDo({required this.id, required this.title, required this.userId, required this.completed});
 
   factory ToDo.fromJson(Map<String,dynamic> json){
-    return ToDo(json["id"], json["title"]);
+    return ToDo(
+      userId: json["userId"],
+      id: json["id"],
+      title: json["title"],
+      completed: json["completed"],
+    );
   }
 
 }

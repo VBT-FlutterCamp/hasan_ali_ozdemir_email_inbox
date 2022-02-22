279a63f054f433be42f16278860c56097059b245
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2022-02-22 14:21:04 +0300

    http service

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -1,13 +1,17 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
+import 'package:main_screen_homework/models/todo.dart';
+
 class ServiceHelper {
   final url = Uri.parse("https://jsonplaceholder.typicode.com/todos");
 
   getTodos()async{
     var _response = await http.get(url);
-    var _decodedResponse = convert.jsonDecode(_response.body) as List;
-    print(_decodedResponse);
-    
+    var _decodedResponses = convert.jsonDecode(_response.body) as List;
+    List<ToDo> _todos = List<ToDo>.generate(_decodedResponses.length, (index){
+      return ToDo.fromJson(_decodedResponses[index]);
+    });
+    return _todos;
   }
 }
